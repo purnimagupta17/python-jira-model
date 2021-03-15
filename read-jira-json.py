53042b0513addb49023fa4912a23a881fe6d0e32
Purnima Gupta <purnimagupta@Purnimas-Air.home>
2021-03-16 00:16:42 +0200

    initial files

---
+++ read-jira-json.py
@@ -0,0 +1,14 @@
+import json
+
+with open('sample.json') as f:
+  data = json.load(f)
+print(data['data']['test3'])
+
+person = '{"name": "Bob", "languages": ["English", "Fench"]}'
+person_dict = json.loads(person)
+
+# Output: {'name': 'Bob', 'languages': ['English', 'Fench']}
+print( person_dict)
+
+# Output: ['English', 'French']
+print(person_dict['languages'])
